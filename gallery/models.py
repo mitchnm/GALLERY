3ddf297651946dbce4035e741b096801faeca97a
mitchnm <mitchndungu21@gmail.com>
2019-06-14 11:21:36 +0300

    created image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model): 
+    img_name = models.CharField(max_length=35)
+    img_decription = models.TextField()
+    img_location = models.ForeignKey(location)
+    img_category = models.ForeignKey(category)
+    article_image = models.ImageField(upload_to = 'gallery/')
+
+    def __str__(self):
+        return self.img_name
