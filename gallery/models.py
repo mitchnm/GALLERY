a2644ebd8ffe79028c8012675e36a5c9df965c7d
mitchnm <mitchndungu21@gmail.com>
2019-06-14 13:02:08 +0300

    creating admin page

--- gallery/models.py
+++ gallery/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Image(models.Model): 
     img_name = models.CharField(max_length=35)
     img_decription = models.TextField()
-    img_location = models.ForeignKey(location)
-    img_category = models.ForeignKey(category)
+    # img_location = models.ForeignKey(location)
+    # img_category = models.ForeignKey(category)
     article_image = models.ImageField(upload_to = 'gallery/')
 
     def __str__(self):
