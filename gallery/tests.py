47279346bd79d592fff7382d740f0572d6e28dee
mitchnm <mitchndungu21@gmail.com>
2019-06-17 08:53:51 +0300

    added test

--- gallery/tests.py
+++ gallery/tests.py
@@ -40,3 +40,43 @@ class CategoryTestClass(TestCase):
    # Testing save method
    def test_save_method(self):
        self.food.save_category()
+
+class ImageTestClass(TestCase):
+       def setUp(self):
+       #creating a new location and saving it
+
+           self.nairobi = Location(location_name='Nairobi')
+           self.nairobi.save_location()
+
+       # creating a new category and saving it
+
+           self.food = Category(category_name='Food')
+           self.food.save_category()
+
+       #creating a new imaage
+
+           self.new_image = Image(image_name ='eat',image_description='eatingafrica',image_category=self.food,image_location=self.nairobi)
+
+
+       #Testing instance for the image
+
+       def test_instance(self):
+           self.assertTrue(isinstance(self.new_image,Image))
+
+       # Testing the save method for the Image class
+       def test_save_method(self):
+           self.new_image.save_image()
+
+       # Testing function to get image searched by category
+       def test_get_category_image(self):
+           category_result = Image.search_by_category()
+           self.assertTrue(len(category_result)>0)
+       # Testing function to get image by id
+       def test_get_image_by_id(self):
+           result_image_id = Image.get_image_by_id(1)
+           self.assertTrue(result_image_id.id,1)
+
+       # Testing function to filter by location
+       def test_filter_by_location(self):
+           filtered_location = Location.objects.get(location_name='Nairobi')
+           self.assertTrue(filtered_location.location_name =='Nairobi')
