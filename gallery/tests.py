8a63ef451c5cee18355f8a60722145b441d45db9
mitchnm <mitchndungu21@gmail.com>
2019-06-17 08:53:00 +0300

    addded tests

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
-
+from .models import Location,Category,Image
 # Create your tests here.
+
+
+
+# REMEMEMBER TO QUERY ALL THE OBJECTS FROM THE DATABASE FOR TETSING EACH CLASS
+#
+# REMEMBER
+# REMEMBER
+class LocationTestClass(TestCase):
+   #Set up method
+
+   def setUp(self):
+       self.nairobi = Location(location_name = 'Nairobi')
+
+   #Testing insatnce
+
+   def test_instance (self):
+       self.assertTrue(isinstance(self.nairobi,Location))
+
+
+   # Testing save method
+   def test_save_method(self):
+       self.nairobi.save_location()
+
+
+class CategoryTestClass(TestCase):
+   #Set up method
+
+   def setUp(self):
+       self.food = Category(category_name = 'Food')
+
+   #Testing insatnce
+
+   def test_instance (self):
+       self.assertTrue(isinstance(self.food,Category))
+
+
+   # Testing save method
+   def test_save_method(self):
+       self.food.save_category()
