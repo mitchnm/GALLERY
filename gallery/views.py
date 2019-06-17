b86ba65ded6b3f62b42282ef416385d38beb2a6c
mitchnm <mitchndungu21@gmail.com>
2019-06-17 08:39:58 +0300

    addded fonts

--- gallery/views.py
+++ gallery/views.py
@@ -15,10 +15,10 @@ def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_articles = Image.search_by_title(search_term)
+        searched_image = Image.search_by_title(search_term)
         message = f"{search_term}"
 
-        return render(request, 'search.html',{"message":message,"images": searched_articles})
+        return render(request, 'search.html',{"message":message,"images": searched_image})
 
     else:
         message = "You haven't searched for any term"
